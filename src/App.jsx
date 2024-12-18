0994f5c626f41be781655469f2d45fd339a94e97
Alexander Salvatierra <22307047@utcgg.edu.mx>
2024-12-17 23:56:21 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import  Header  from "./components/Header"
+import  Footer  from "./components/Footer"
+import Guitar  from "./components/Guitar"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+       <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+          <Guitar />
+        </div>
+      </main>
+
+
+      <Footer />
+    </>
+  )
+}
